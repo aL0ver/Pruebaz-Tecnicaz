2258e31818bf95ddcef351457b1b844dcf2ec907
LuckyK <88355964+aL0ver@users.noreply.github.com>
2022-07-24 14:25:14 -0500

    Update isNum.py

--- isNum.py
+++ isNum.py
@@ -1,11 +1,7 @@
 def isNum(entrada):
-    count = len(entrada)
     for i in entrada:
         try: 
             int(i)
         except ValueError:
-            return False
-        else:
-            count -= 1
-            if count == 0:
-                return True
+            return False    
+    return True
